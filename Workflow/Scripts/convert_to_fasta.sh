7b339463788f9b930df05390d8e4f834693992a8
IIeaf <yaprak_1x1@hotmail.com>
2022-04-13 12:51:33 +0200

    Changed repository structure

---
+++ Workflow/Scripts/convert_to_fasta.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#mkdir ../Data && cd $_
+export PATH=$PATH:/home/ubuntu/Desktop/Dataprocessing/Resources/sratoolkit.2.11.2-ubuntu64/bin
+fasterq-dump SRR5646572 SRR5646573 SRR5646574 SRR5646575
---
+++ Workflow/Scripts/download_sra.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH=$PATH:/home/ubuntu/Desktop/Dataprocessing/Resources/sratoolkit.2.11.2-ubuntu64/bin
+prefetch SRR5646572 SRR5646573 SRR5646574 SRR5646575
+
